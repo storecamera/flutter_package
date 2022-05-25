0df16f63b330b0e98be8873a4a456f1a875320d6
zpdl9089 <zpdl9089@storecamera.io>
2022-05-25 15:45:25 +0900

    contract_value

--- contract/lib/contract.dart
+++ contract/lib/contract.dart
@@ -2,6 +2,7 @@ library contract;
 
 export 'src/contract.dart';
 export 'src/contract_observer.dart';
+export 'src/contract_value.dart';
 export 'src/exceptions.dart';
 export 'src/service.dart';
 export 'src/service_view.dart';
