b92dc71cf600d1313564215d0c51200d1b5f43b3
zpdl9089 <zpdl9089@storecamera.io>
2022-06-30 15:32:40 +0900

    store_camera_widget : image url

---
+++ store_camera_widget/lib/widget/image.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+import 'package:store_camera_widget/painting/edge_insets.dart';
+
+class ImageUrlWidget extends StatelessWidget {
+  final String? url;
+
+  final double? width;
+  final double? height;
+  final double? size;
+  final BoxFit fit;
+  final Widget? loading;
+  final Color? loadingColor;
+  final Widget? error;
+  final Color? errorColor;
+
+  const ImageUrlWidget({
+    super.key,
+    this.url,
+    this.width,
+    this.height,
+    this.size,
+    required this.fit,
+    this.loading,
+    this.loadingColor,
+    this.error,
+    this.errorColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return url != null
+        ? Image.network(
+            url!,
+            width: width ?? size,
+            height: width ?? size,
+            fit: fit,
+            loadingBuilder: (
+              BuildContext context,
+              Widget child,
+              ImageChunkEvent? loadingProgress,
+            ) {
+              if (loadingProgress == null) {
+                return child;
+              }
+              return loading ??
+                  ImageLoading(
+                    color: loadingColor,
+                  );
+            },
+            errorBuilder: (
+              context,
+              error,
+              stackTrace,
+            ) =>
+                this.error ?? ImageError(color: errorColor),
+          )
+        : error ?? ImageError(color: errorColor);
+  }
+}
+
+class ImageLoading extends StatelessWidget {
+  final Color? color;
+
+  const ImageLoading({super.key, this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Icon(
+      Icons.downloading,
+      color: color ?? Theme.of(context).hintColor,
+    ));
+  }
+}
+
+class ImageError extends StatelessWidget {
+  final Color? color;
+
+  const ImageError({super.key, this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Icon(
+      Icons.image_not_supported,
+      color: color ?? Theme.of(context).hintColor,
+    ));
+  }
+}
