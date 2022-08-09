8c50f0db7ee0864894750be14886c373c07d281e
zpdl9089 <zpdl9089@storecamera.io>
2022-08-09 11:24:16 +0900

    store_camera_widget : dropdown

--- store_camera_widget/lib/widget/dropdown.dart
+++ store_camera_widget/lib/widget/dropdown.dart
@@ -27,25 +27,25 @@ class ScDropdownButton<T> extends StatelessWidget {
       value: value,
       items: items
           .map((e) => DropdownMenuItem<T>(
-        value: e,
-        child: Padding(
-          padding: padding,
-          child: Text(
-            valueToString(e),
-            style: e == value
-                ? theme.textTheme.bodyLarge
-                ?.copyWith(color: theme.colorScheme.primary)
-                : theme.textTheme.bodyLarge,
-          ),
-        ),
-      ))
+                value: e,
+                child: Padding(
+                  padding: padding,
+                  child: Text(
+                    valueToString(e),
+                    style: e == value
+                        ? theme.textTheme.bodyLarge
+                            ?.copyWith(color: theme.colorScheme.primary)
+                        : theme.textTheme.bodyLarge,
+                  ),
+                ),
+              ))
           .toList(),
       onChanged: enabled
           ? (_) {
-        if (_ != null) {
-          onChanged.call(_);
-        }
-      }
+              if (_ != null) {
+                onChanged.call(_);
+              }
+            }
           : null,
     );
   }
@@ -83,7 +83,7 @@ class ScDropdownFormField<T> extends StatelessWidget {
       value: value,
       decoration: InputDecoration(
         constraints:
-        maxWidth != null ? BoxConstraints(maxWidth: maxWidth!) : null,
+            maxWidth != null ? BoxConstraints(maxWidth: maxWidth!) : null,
         labelText: labelText,
         errorText: errorText,
         contentPadding: padding,
@@ -103,7 +103,7 @@ class ScDropdownFormField<T> extends StatelessWidget {
           text,
           style: e == value
               ? theme.textTheme.bodyLarge
-              ?.copyWith(color: theme.colorScheme.primary)
+                  ?.copyWith(color: theme.colorScheme.primary)
               : theme.textTheme.bodyLarge,
         );
         return DropdownMenuItem<T>(
@@ -113,11 +113,76 @@ class ScDropdownFormField<T> extends StatelessWidget {
       }).toList(),
       onChanged: enabled
           ? (_) {
-        if (_ != null) {
-          onChanged.call(_);
-        }
-      }
+              if (_ != null) {
+                onChanged.call(_);
+              }
+            }
           : null,
+    );
+  }
+}
+
+class ScDropdownFormNullableField<T> extends StatelessWidget {
+  final T? value;
+  final double? maxWidth;
+  final String? labelText;
+  final String? errorText;
+  final bool enabled;
+  final EdgeInsetsGeometry padding;
+  final Iterable<T?> items;
+  final String Function(T?) valueToString;
+  final ValueChanged<T?> onChanged;
+
+  const ScDropdownFormNullableField({
+    Key? key,
+    this.value,
+    this.maxWidth,
+    this.labelText,
+    this.errorText,
+    this.padding = contentPadding,
+    this.enabled = true,
+    required this.items,
+    required this.valueToString,
+    required this.onChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return DropdownButtonFormField<T?>(
+      value: value,
+      decoration: InputDecoration(
+        constraints:
+            maxWidth != null ? BoxConstraints(maxWidth: maxWidth!) : null,
+        labelText: labelText,
+        errorText: errorText,
+        contentPadding: padding,
+      ),
+      isExpanded: true,
+      selectedItemBuilder: (context) => items.map((e) {
+        final text = valueToString(e);
+        final child = Text(
+          text,
+          style: theme.textTheme.bodyLarge,
+        );
+        return text.isNotEmpty ? FittedBox(child: child) : child;
+      }).toList(),
+      items: items.map((e) {
+        final text = valueToString(e);
+        final child = Text(
+          text,
+          style: e == value
+              ? theme.textTheme.bodyLarge
+                  ?.copyWith(color: theme.colorScheme.primary)
+              : theme.textTheme.bodyLarge,
+        );
+        return DropdownMenuItem<T?>(
+          value: e,
+          child: text.isNotEmpty ? FittedBox(child: child) : child,
+        );
+      }).toList(),
+      onChanged: enabled ? onChanged : null,
     );
   }
 }
@@ -133,7 +198,7 @@ class ScDropdownFormFieldOtherString extends StatelessWidget {
   final String otherItem;
 
   final Future<dynamic> Function(BuildContext context, String value)?
-  onOtherValue;
+      onOtherValue;
   final void Function(String value) onChanged;
 
   const ScDropdownFormFieldOtherString({
@@ -162,7 +227,7 @@ class ScDropdownFormFieldOtherString extends StatelessWidget {
       value: value,
       decoration: InputDecoration(
         constraints:
-        maxWidth != null ? BoxConstraints(maxWidth: maxWidth!) : null,
+            maxWidth != null ? BoxConstraints(maxWidth: maxWidth!) : null,
         labelText: labelText,
         errorText: errorText,
         contentPadding: padding,
@@ -181,7 +246,7 @@ class ScDropdownFormFieldOtherString extends StatelessWidget {
           e,
           style: e == value
               ? theme.textTheme.bodyLarge
-              ?.copyWith(color: theme.colorScheme.primary)
+                  ?.copyWith(color: theme.colorScheme.primary)
               : theme.textTheme.bodyLarge,
         );
         return DropdownMenuItem<String>(
@@ -191,30 +256,30 @@ class ScDropdownFormFieldOtherString extends StatelessWidget {
       }).toList(),
       onChanged: enabled
           ? (_) async {
-        if (_ == null) {
-          return;
-        }
-        String changedValue = _;
-        if (changedValue == otherItem) {
-          if (onOtherValue != null) {
-            final result =
-            await onOtherValue!.call(context, this.value ?? '');
-            if (result is String) {
-              changedValue = result;
-            }
-          } else {
-            final result = await scSingleTextEditDialog(context,
-                title: labelText, initText: this.value);
-            if (result is String) {
-              changedValue = result;
-            }
-          }
-        }
+              if (_ == null) {
+                return;
+              }
+              String changedValue = _;
+              if (changedValue == otherItem) {
+                if (onOtherValue != null) {
+                  final result =
+                      await onOtherValue!.call(context, this.value ?? '');
+                  if (result is String) {
+                    changedValue = result;
+                  }
+                } else {
+                  final result = await scSingleTextEditDialog(context,
+                      title: labelText, initText: this.value);
+                  if (result is String) {
+                    changedValue = result;
+                  }
+                }
+              }
 
-        if (changedValue.isNotEmpty) {
-          onChanged(changedValue);
-        }
-      }
+              if (changedValue.isNotEmpty) {
+                onChanged(changedValue);
+              }
+            }
           : null,
     );
   }
