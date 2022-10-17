8739aaba2ca64cd77acae8e24721bda13bb7ee9a
zpdl9089 <zpdl9089@storecamera.io>
2022-10-17 15:53:30 +0900

    image

--- store_camera_widget/lib/widget/image.dart
+++ store_camera_widget/lib/widget/image.dart
@@ -27,62 +27,86 @@ class ImageUrlWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return url != null
-        ? Image.network(
-            url!,
-            width: width ?? size,
-            height: width ?? size,
-            fit: fit,
-            loadingBuilder: (
-              BuildContext context,
-              Widget child,
-              ImageChunkEvent? loadingProgress,
-            ) {
-              if (loadingProgress == null) {
-                return child;
-              }
-              return loading ??
-                  ImageLoading(
-                    color: loadingColor,
-                  );
-            },
-            errorBuilder: (
-              context,
-              error,
-              stackTrace,
-            ) =>
-                this.error ?? ImageError(color: errorColor),
-          )
-        : error ?? ImageError(color: errorColor);
+    final url = this.url;
+    final width = this.width ?? size;
+    final height = this.height ?? size;
+
+    if (url == null || url.trim().isEmpty) {
+      return error ??
+          ImageNotSupported(
+            width: width,
+            height: height,
+            color: errorColor,
+          );
+    }
+    return Image.network(
+      url,
+      width: width,
+      height: height,
+      fit: fit,
+      loadingBuilder: (
+        BuildContext context,
+        Widget child,
+        ImageChunkEvent? loadingProgress,
+      ) {
+        if (loadingProgress == null) {
+          return child;
+        }
+        return loading ??
+            ImageLoading(
+              width: width,
+              height: height,
+              color: loadingColor,
+            );
+      },
+      errorBuilder: (
+        context,
+        error,
+        stackTrace,
+      ) =>
+          this.error ??
+          ImageNotSupported(width: width, height: height, color: errorColor),
+    );
   }
 }
 
 class ImageLoading extends StatelessWidget {
+  final double? width;
+  final double? height;
   final Color? color;
 
-  const ImageLoading({super.key, this.color});
+  const ImageLoading({super.key, this.width, this.height, this.color});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return Container(
+        width: width,
+        height: height,
+        alignment: AlignmentDirectional.center,
         child: Icon(
-      Icons.downloading,
-      color: color ?? Theme.of(context).hintColor,
-    ));
+          Icons.downloading,
+          color: color ?? Theme.of(context).hintColor,
+        ));
   }
 }
 
-class ImageError extends StatelessWidget {
+class ImageNotSupported extends StatelessWidget {
+  final double? width;
+  final double? height;
   final Color? color;
 
-  const ImageError({super.key, this.color});
+  const ImageNotSupported({super.key, this.width, this.height, this.color});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Icon(
-      Icons.image_not_supported,
-      color: color ?? Theme.of(context).hintColor,
-    ));
+    return Container(
+      width: width,
+      height: height,
+      alignment: AlignmentDirectional.center,
+      child: Icon(
+        Icons.image_not_supported,
+        color: color ?? Theme.of(context).hintColor,
+      ),
+    );
   }
 }
