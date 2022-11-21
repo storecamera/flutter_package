27525dbd34445985552c547c02a1c13cbd50e8c0
zpdl9089 <zpdl9089@storecamera.io>
2022-11-21 16:25:18 +0900

    store_camera_template

--- store_camera_template/lib/log.dart
+++ store_camera_template/lib/log.dart
@@ -6,26 +6,38 @@ import 'package:flutter/foundation.dart';
 var logLevel = 3;
 
 // ignore: camel_case_types
-class log {
-  static void v(Object? object) {
-    if (kDebugMode) {
-      if(logLevel >= 3) {
-        print(object);
+typedef log = Log;
+
+class Log {
+  static void v(Object? object, [bool printTime = false]) {
+    if(logLevel >= 3) {
+      _printMsg(object, printTime);
+    }
+  }
+
+  static void i(Object? object, [bool printTime = false]) {
+    if(logLevel >= 2) {
+      _printMsg(object, printTime);
+    }
+  }
+
+  static void e(Object? object, [bool printTime = false]) {
+    _printMsg(object, printTime);
+  }
+
+  static void _printMsg(Object? msg, bool printTime) {
+    if(printTime) {
+      if (kDebugMode) {
+        print('${_time()} : $msg');
+      }
+    } else {
+      if (kDebugMode) {
+        print(msg);
       }
     }
   }
 
-  static void i(Object? object) {
-    if (kDebugMode) {
-      if(logLevel >= 2) {
-        print(object);
-      }
-    }
-  }
-
-  static void e(Object? object) {
-    if (kDebugMode) {
-      print(object);
-    }
+  static String _time(){
+    return DateTime.now().toString();
   }
 }
--- store_camera_template/lib/store_camera_template.dart
+++ store_camera_template/lib/store_camera_template.dart
@@ -1,14 +1,8 @@
 library store_camera_template;
 
-export 'package:contract/contract.dart';
-export 'package:store_camera_widget/store_camera_widget.dart';
-
-export 'extension/contract_extension.dart';
 export 'extension/datetime_extension.dart';
 export 'extension/enum_extionsion.dart';
 export 'extension/list_extension.dart';
 export 'extension/map_extension.dart';
-export 'extension/rx_extension.dart';
 export 'exception/exceptions.dart';
 export 'log.dart';
-export 'links.dart';
